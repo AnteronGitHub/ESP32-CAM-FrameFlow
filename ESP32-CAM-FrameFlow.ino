f4d82e417e265e7a02a7e1cb40323cd258b052ed
Antero Vainio <anterongugl@gmail.com>
2024-07-27 12:57:58 +0300

    Implement image cropping for png

--- ESP32-CAM-FrameFlow.ino
+++ ESP32-CAM-FrameFlow.ino
@@ -2,6 +2,7 @@
 #include <WiFi.h>
 #include "esp_transport_tcp.h"
 #include <stdio.h>
+#include <inttypes.h>
 
 #include "camera_pins.h"
 #include "frame_flow.c"
@@ -32,12 +33,12 @@ void setup() {
   config.pin_pwdn = PWDN_GPIO_NUM;
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
-  config.frame_size = FRAMESIZE_HD;
-  config.pixel_format = PIXFORMAT_JPEG;
+  config.frame_size = FRAMESIZE_VGA;
+  config.pixel_format = PIXFORMAT_RGB565;
   config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
   config.fb_location = CAMERA_FB_IN_PSRAM;
   config.jpeg_quality = 12;
-  config.fb_count = 1;
+  config.fb_count = 3;
 
   esp_err_t err = esp_camera_init(&config);
   if (err != ESP_OK) {
@@ -61,28 +62,44 @@ void setup() {
   // Stream video
   camera_fb_t* fb;
   char* read_buffer = (char*) malloc(4);
+  size_t _jpg_buf_len = 0;
+  uint8_t *_jpg_buf = NULL;
+
   while (true) {
     int64_t fr_start = esp_timer_get_time();
 
-    while (true) {
-      fb = esp_camera_fb_get();
-      if (!fb) {
-        Serial.println("Unable to read frame buffer");
-      } else {
-        break;
-      }
+    while (!(fb = esp_camera_fb_get())) {
+      Serial.println("Unable to read frame buffer");
     }
+    int64_t fr_captured = esp_timer_get_time();
 
-    if (send_frame(transport, fb) == -1) {
+    crop_png(fb, 200, 200, 200, 200);
+    int64_t fr_cropped = esp_timer_get_time();
+
+    fmt2jpg(fb->buf, fb->len, fb->width, fb->height, config.pixel_format, 80, &_jpg_buf, &_jpg_buf_len);
+    int64_t fr_converted = esp_timer_get_time();
+
+    if (send_frame(transport, _jpg_buf, _jpg_buf_len) == -1) {
       break;
     }
+    int64_t fr_sent = esp_timer_get_time();
 
     esp_camera_fb_return(fb);
+    free(_jpg_buf);
+    _jpg_buf = NULL;
+
+    int64_t fr_buffers_managed = esp_timer_get_time();
 
     frame_flow_server_feedback_t feedback = recv_feedback(transport, read_buffer);
-    printf("%d\n", feedback.roi_x);
 
-    int64_t ms_elapsed = (esp_timer_get_time() - fr_start) / 1000;
+    int64_t fr_feedback = esp_timer_get_time();
+
+    printf("capture: %" PRIu64, (fr_captured - fr_start) / 1000);
+    printf(", crop: %" PRIu64, (fr_cropped - fr_captured) / 1000);
+    printf(", conversion: %" PRIu64, (fr_converted - fr_cropped) / 1000);
+    printf(", transmission: %" PRIu64, (fr_sent - fr_converted) / 1000);
+    printf(", buffer management: %" PRIu64, (fr_buffers_managed - fr_sent) / 1000);
+    printf(", feedback received: %" PRIu64 "\n", (fr_feedback - fr_buffers_managed) / 1000);
   }
 
   disconnect_from_server(transport);
--- frame_flow.c
+++ frame_flow.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #include "esp_camera.h"
 #include "esp_transport_tcp.h"
 
@@ -40,14 +42,14 @@ void disconnect_from_server(esp_transport_handle_t t) {
  * @param[in]  fb  The camera frame buffer handle
  *
  */
-int send_frame(esp_transport_handle_t t, camera_fb_t* fb) {
+int send_frame(esp_transport_handle_t t, uint8_t * buf, size_t len) {
   // Send the header
-  if (esp_transport_write(t, (char *) &fb->len, 4, 1000) == -1) {
+  if (esp_transport_write(t, (char *) &len, 4, 1000) == -1) {
     return -1;
   }
 
   // Send the frame
-  if (esp_transport_write(t, (char *) fb->buf, fb->len, 1000) == -1) {
+  if (esp_transport_write(t, (char *) buf, len, 1000) == -1) {
     return -1;
   }
 
@@ -59,4 +61,45 @@ frame_flow_server_feedback_t recv_feedback(esp_transport_handle_t t, char* rx_bu
   unsigned int number = *(unsigned int *) rx_buffer;
   frame_flow_server_feedback_t feedback = { .roi_x = number };
   return feedback;
+}
+
+/**
+ * Implementation copied from: https://simplyexplained.com/blog/esp32-cam-cropping-images-on-device/
+ */
+void crop_png(camera_fb_t *fb, unsigned short cropLeft, unsigned short cropRight, unsigned short cropTop, unsigned short cropBottom)
+{
+    unsigned int maxTopIndex = cropTop * fb->width * 2;
+    unsigned int minBottomIndex = ((fb->width*fb->height) - (cropBottom * fb->width)) * 2;
+    unsigned short maxX = fb->width - cropRight; // In pixels
+    unsigned short newWidth = fb->width - cropLeft - cropRight;
+    unsigned short newHeight = fb->height - cropTop - cropBottom;
+    size_t newJpgSize = newWidth * newHeight *2;
+
+    unsigned int writeIndex = 0;
+    // Loop over all bytes
+    for(int i = 0; i < fb->len; i+=2){
+        // Calculate current X, Y pixel position
+        int x = (i/2) % fb->width;
+
+        // Crop from the top
+        if(i < maxTopIndex){ continue; }
+
+        // Crop from the bottom
+        if(i > minBottomIndex){ continue; }
+
+        // Crop from the left
+        if(x <= cropLeft){ continue; }
+
+        // Crop from the right
+        if(x > maxX){ continue; }
+
+        // If we get here, keep the pixels
+        fb->buf[writeIndex++] = fb->buf[i];
+        fb->buf[writeIndex++] = fb->buf[i+1];
+    }
+
+    // Set the new dimensions of the framebuffer for further use.
+    fb->width = newWidth;
+    fb->height = newHeight;
+    fb->len = newJpgSize;
 }
